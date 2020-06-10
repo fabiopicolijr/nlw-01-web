f185e5d5313dd3e0d4a63042a33fb83e463bfe58
Fabio Picoli Jr <fabiopicolijr@gmail.com>
2020-06-09 21:28:11 -0300

    background fix

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FiLogIn} from 'react-icons/fi'
+import {FiLogIn} from 'react-icons/fi';
 import {Link} from 'react-router-dom';
 
 import './styles.css';
@@ -10,10 +10,7 @@ const Home = () => {
   return(
     <div id="page-home">
       <div className="content">
-        <header>
-          <img src={logo} alt="Ecoleta"/>
-        </header>
-
+        <header><img src={logo} alt="Ecoleta"/></header>
         <main>
           <h1>Seu marketplace de coletas de residuos.</h1>
           <p>Ajudamos as pessoas a encontrarem pontos de coleta de forma eficiente.</p>
@@ -27,8 +24,7 @@ const Home = () => {
         </main>
       </div>
     </div>
-
   );
-}
+};
 
 export default Home;
